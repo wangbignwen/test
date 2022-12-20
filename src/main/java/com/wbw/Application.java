a8764f0649df3e575b4b1cd179402dcb37419a04
王炳文 <654736229@qq.com>
2022-12-20 10:46:32 +0800

    测试

--- src/main/java/com/wbw/Application.java
+++ src/main/java/com/wbw/Application.java
@@ -11,6 +11,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
         System.out.println("===============spring boot start==============");
+        //
     }
 }
 
