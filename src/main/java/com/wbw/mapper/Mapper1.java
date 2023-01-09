c56656ee2e4d11124bb57139a27a7d6bb3f1cdb1
王炳文 <654736229@qq.com>
2023-01-09 20:57:47 +0800

    测试

--- src/main/java/com/wbw/mapper/Mapper1.java
+++ src/main/java/com/wbw/mapper/Mapper1.java
@@ -4,7 +4,12 @@ import com.wbw.pojo.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+import java.util.Map;
+
 @Mapper
 public interface Mapper1 {
     Integer add(@Param("param") User user);
+
+    List<Map<String, String>> get(@Param("id") String id);
 }
