1ae4ebf5c90005b7d2e727bc866149d7c29a1a3c
王炳文 <654736229@qq.com>
2023-04-05 15:41:36 +0800

    4

--- src/main/java/com/wbw/Application.java
+++ src/main/java/com/wbw/Application.java
@@ -8,7 +8,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync // 开启 Spring 异步的功能
-@MapperScan("com.wbw.mapper")  //dao层可以省略@Mapper注解
+@MapperScan("com.wbw.mapper")  //扫dao层，有该注解时dao层可以省略@Mapper注解
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
